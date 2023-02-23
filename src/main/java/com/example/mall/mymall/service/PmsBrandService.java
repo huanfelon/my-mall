17fc8d4899fa8a165b697df9a302f9c22b139506
皇飞龙 <2291267989@qq.com>
2023-02-23 14:04:19 +0800

    配置好mybatis生成代码，以及分页

---
+++ src/main/java/com/example/mall/mymall/service/PmsBrandService.java
@@ -0,0 +1,23 @@
+package com.example.mall.mymall.service;
+
+
+import com.example.mall.mymall.mbg.model.PmsBrand;
+
+import java.util.List;
+
+/**
+ * PmsBrandService
+ */
+public interface PmsBrandService {
+    List<PmsBrand> listAllBrand();
+
+    int createBrand(PmsBrand brand);
+
+    int updateBrand(Long id, PmsBrand brand);
+
+    int deleteBrand(Long id);
+
+    List<PmsBrand> listBrand(int pageNum, int pageSize);
+
+    PmsBrand getBrand(Long id);
+}
